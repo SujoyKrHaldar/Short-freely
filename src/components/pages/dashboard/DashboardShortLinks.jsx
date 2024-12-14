891b9cdd8782c05bc8010e266c642510bcf7fb08
SujoyKrHaldar <sujoykrhaldar@gmail.com>
2024-12-14 12:50:11 +0530

    Add dashboard breadcrumbs

--- src/components/pages/dashboard/DashboardShortLinks.jsx
+++ src/components/pages/dashboard/DashboardShortLinks.jsx
@@ -1,15 +1,24 @@
 import { Link } from "react-router-dom";
 import DashboardEmptyBox from "./shared/DashboardEmptyBox";
+import DashboardBreadcrumb from "./shared/DashboardBreadcrumb";
 
 function DashboardShortLinks() {
+  const breadcrumbs = [
+    {
+      name: "Dashboard",
+      url: "/dashboard",
+    },
+    {
+      name: "Links",
+      url: "/dashboard/links",
+    },
+  ];
+
   return (
     <section className="w-full h-full space-y-8">
       <div className="flex items-end justify-between w-full h-full">
         <div className="space-y-2">
-          <div className="flex items-center gap-2 text-zinc-500">
-            <Link to="/dashboard">Dashboard</Link> /{" "}
-            <Link to="/dashboard/links">Links</Link>
-          </div>
+          <DashboardBreadcrumb links={breadcrumbs} />
           <h1 className="text-4xl font-bold">All Short Links</h1>
           <p className="text-lg">
             Create and manage all short links from here.
