0af7a3fc701b64c22ab35849b39ad68ec01b0ab3
SujoyKrHaldar <sujoykrhaldar@gmail.com>
2024-12-15 15:03:47 +0530

    update url strings as queryparams

--- src/routes/AuthRoutes.jsx
+++ src/routes/AuthRoutes.jsx
@@ -6,7 +6,9 @@ function AuthRoutes() {
   const { authStatus } = useAuth();
   const queryParams = useQueryParams("redirectTo");
 
-  const redirectRoute = queryParams ? `${queryParams}` : "/dashboard";
+  const redirectRoute = queryParams
+    ? `${queryParams.replaceAll("%2F", "/").replace("%3A", ":")}`
+    : "/dashboard";
 
   if (authStatus) return <Navigate to={redirectRoute} />;
 
