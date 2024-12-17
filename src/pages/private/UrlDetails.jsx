f9adab732ff1005bb6c565d3ddc41fa7d5ab38cd
SujoyKrHaldar <sujoykrhaldar@gmail.com>
2024-12-17 20:04:57 +0530

    update url details page

--- src/pages/private/UrlDetails.jsx
+++ src/pages/private/UrlDetails.jsx
@@ -1,13 +1,11 @@
-/* eslint-disable no-unused-vars */
-import { useEffect, useState } from "react";
 import { DashboardSingleUrlPage } from "../../components/pages/dashboard";
 import { MetaTags } from "../../components/shared";
-import { getUrlById } from "../../api/urlService";
 import { useParams } from "react-router-dom";
-import { useAuth } from "../../hooks";
+import { useFetchUrlById } from "../../hooks";
 
 function UrlDetails() {
-  const { error, data, loading } = useFetchUrlById();
+  const { urlId } = useParams();
+  const { error, data, loading } = useFetchUrlById(urlId);
 
   return (
     <>
@@ -24,36 +22,5 @@ function UrlDetails() {
     </>
   );
 }
-
-const useFetchUrlById = () => {
-  const [data, setData] = useState();
-  const [error, setError] = useState(false);
-  const [loading, setLoading] = useState(true);
-  const { urlId } = useParams();
-  const { userData } = useAuth();
-  const userId = userData?.$id;
-
-  const fetchUrlData = async () => {
-    setLoading(true);
-    setError(false);
-    try {
-      const response = await getUrlById(urlId);
-      if (userId !== response.userId) {
-        return setError(true);
-      }
-      setData(response);
-    } catch (_) {
-      setError(true);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    fetchUrlData();
-  }, [urlId, userId]);
-
-  return { loading, data, error };
-};
 
 export default UrlDetails;
