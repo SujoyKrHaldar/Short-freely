20e8e7a478a7c586528e5135f3f8e2564070e53f
SujoyKrHaldar <sujoykrhaldar@gmail.com>
2024-12-18 02:29:50 +0530

    add logout btn

--- src/components/pages/dashboard/account/DashboardAccount.jsx
+++ src/components/pages/dashboard/account/DashboardAccount.jsx
@@ -1,3 +1,4 @@
+import { useLogout } from "../../../../hooks";
 import DashboardBreadcrumb from "../shared/DashboardBreadcrumb";
 import DashboardPasswordUpdate from "./DashboardPasswordUpdate";
 import DashboardProfileEdit from "./DashboardProfileEdit";
@@ -14,16 +15,26 @@ function DashboardAccount() {
       url: "/dashboard/account",
     },
   ];
+  const { logout } = useLogout();
 
   return (
     <section className="w-full h-full space-y-8">
-      <div className="space-y-2">
-        <DashboardBreadcrumb links={breadcrumbs} />
-        <h1 className="text-4xl font-bold">Your Account</h1>
-        <p className="text-lg">Manage your account.</p>
+      <div className="flex items-end justify-between">
+        <div className="space-y-2">
+          <DashboardBreadcrumb links={breadcrumbs} />
+          <h1 className="text-4xl font-bold">Your Account</h1>
+          <p className="text-lg">Manage your account.</p>
+        </div>
+
+        <button
+          onClick={logout}
+          className="bg-red-700 border text-white border-red-700 px-6 py-2 cursor-pointer"
+        >
+          Logout Account
+        </button>
       </div>
 
-      <div className="space-y-8">
+      <div className="space-y-8 pb-8">
         <DashboardProfileEdit />
         <DashboardPasswordUpdate />
         <DashboardSessions />
