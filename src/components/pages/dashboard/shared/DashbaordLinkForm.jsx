15ff8c60c808d403e2f70156df616460fad21a13
SujoyKrHaldar <sujoykrhaldar@gmail.com>
2024-12-19 04:00:19 +0530

    update create and edit form

--- src/components/pages/dashboard/shared/DashbaordLinkForm.jsx
+++ src/components/pages/dashboard/shared/DashbaordLinkForm.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
@@ -7,7 +6,10 @@ import { useNavigate, useParams } from "react-router-dom";
 import Input from "../../../ui/forms/Input";
 import { createUrl, updateUrlById } from "../../../../api/urlService";
 import { responseErrorType, responseStatus } from "../../../../utils/constants";
+import { Lock, QrCode } from "lucide-react";
+import ShareLinkPopup from "./ShareLinkPopup";
 
+// TODO: Make seperate custom hooks for each and every useeffct and functions
 const DashbaordLinkForm = ({ defaultData }) => {
   const {
     register,
@@ -18,74 +20,74 @@ const DashbaordLinkForm = ({ defaultData }) => {
     formState: { errors, isDirty },
   } = useForm();
 
-  const [qrVisible, setQrVisible] = useState(false);
   const [loading, setLoading] = useState(false);
   const [qrCode, setQrCode] = useState("");
-  // const [expirationVisible, setExpirationVisible] = useState(false);
   const [isEditMode, setIsEditMode] = useState(false);
+  const [isEnableSharePopup, setEnableSharePopup] = useState(false);
+  const [shareDetails, setShareDetails] = useState({});
 
-  const longurlFromQuery = useQueryParams("longurl");
   const navigate = useNavigate();
   const notify = useNotification();
   const { userData } = useAuth();
-  const userId = userData.$id;
   const { urlId } = useParams();
+  const userId = userData.$id;
 
-  // ! POPULATE EXISTING DATA IF IN EDIT MODE
+  // * POPULATE EXISTING DATA IF IN EDIT MODE
   useEffect(() => {
     if (defaultData) {
+      const { title, originalUrl, qrCode: defaultQrCode } = defaultData;
       setIsEditMode(true);
-      const { title, originalUrl, customSlug, expiration, expirationDate } =
-        defaultData;
       setValue("title", title);
       setValue("originalUrl", originalUrl);
-      setValue("customSlug", customSlug);
-      setValue("expiration", expiration);
-      setValue("expirationDate", expirationDate);
-      // setExpirationVisible(Boolean(expiration));
+      setQrCode(defaultQrCode);
     } else {
       setIsEditMode(false);
       reset();
     }
   }, [defaultData, setValue, reset]);
 
-  // ! GETTING LONGURL DATA FROM QUERY PARAMS WHICH IC COMING FROM HOMEPAGE
+  // * GETTING LONGURL DATA FROM QUERY PARAMS WHICH IC COMING FROM HOMEPAGE
+  const longurlFromQuery = useQueryParams("longurl");
+
   useEffect(() => {
-    if (longurlFromQuery) {
+    if (longurlFromQuery && !isEditMode) {
       setValue("originalUrl", longurlFromQuery);
     }
-  }, [longurlFromQuery, setValue]);
+  }, [longurlFromQuery, setValue, isEditMode]);
 
-  // TODO: WORK ON QR GENERATEION url: https://chatgpt.com/c/675d491c-4ecc-8007-bdae-9306e2ae7d3a
-  // TODO: DISABLE UPDATE THE EXISTING DATA
-  // TODO: ALSO WORK ON FAVICON GENERATION FOR LONG URLS
-  const handleQrToggle = (e) => {
-    setQrVisible(e.target.checked);
-    const url = watch("originalUrl");
-    if (e.target.checked) {
-      if (url) {
-        setQrCode(`https://api.qrserver.com/v1/create-qr-code/?data=${url}`);
-      }
+  // * GENERATE QR CODE IN REALTIME WITH LONG URL
+  const url = watch("originalUrl");
+
+  useEffect(() => {
+    if (url) {
+      setQrCode(`https://api.qrserver.com/v1/create-qr-code/?data=${url}`);
     } else {
       setQrCode("");
     }
-  };
+  }, [url]);
 
   // * CANCLE SUBMISSION FOR EDIT AND CREATE
   const handleCancle = () => {
-    setQrVisible(false);
-    // setExpirationVisible(false);
     if (isEditMode) {
       reset(defaultData);
+      setQrCode(defaultData.qrCode);
       return;
     }
-    navigate("/dashboard/create");
+    if (longurlFromQuery) navigate("/dashboard/create");
+    setQrCode("");
     reset();
   };
 
   // * GENERATE CUSTOM SLUG FOR SHORT URL
   const generateSlug = () => {
     return Math.random().toString(36).substring(2, 8);
+  };
+
+  // * GENERATE WEBSITE FAVICON FROM ORIGINAL URL
+  const generateFavicon = (originalUrl) => {
+    const urlHostname = new URL(originalUrl).hostname;
+    const faviconUrl = `https://www.google.com/s2/favicons?sz=64&domain=${urlHostname}`;
+    return { urlHostname, faviconUrl };
   };
 
   // * ON FORM SUBMIT
@@ -99,7 +101,7 @@ const DashbaordLinkForm = ({ defaultData }) => {
     }
   };
 
-  //! CREATE NEW DOCUMENT
+  //* CREATE NEW DOCUMENT
   const createNewLinkUrl = async (data) => {
     const SHORT_URL_DOMAIN = "short.ly";
 
@@ -108,81 +110,55 @@ const DashbaordLinkForm = ({ defaultData }) => {
       data.customSlug = slug;
     }
 
-    data.userId = userId;
-
     data.shortUrl = SHORT_URL_DOMAIN + "/" + data.customSlug;
 
+    const { urlHostname, faviconUrl } = generateFavicon(data.originalUrl);
+
+    const newUrl = {
+      shortUrl: data.shortUrl,
+      title: data.title,
+      customSlug: data.customSlug,
+      originalUrl: data.originalUrl,
+      userId,
+      originalUrlDomain: urlHostname,
+      faviconUrl,
+      qrCode: qrCode,
+    };
+
     try {
-      const response = await createUrl({
-        shortUrl: data.shortUrl,
-        title: data.title,
-        customSlug: data.customSlug,
-        originalUrl: data.originalUrl,
-        userId: data.userId,
-      });
-
+      const response = await createUrl(newUrl);
       if (response) {
-        notify({
-          message: "Short url created successfully.",
-          type: responseStatus.SUCCESS,
-          timeout: 5000,
-        });
         reset();
-
-        //TODO : ADD A SHARE QR & URL POPUP WITH SMALL ANIMATION REACT TSPARTICLE https://github.com/tsparticles/react/#readme
-        navigate("/dashboard/link/" + urlId, { replace: true });
+        setEnableSharePopup(true);
+        setShareDetails({
+          qrCode: response.qrCode,
+          shortUrl: response.shortUrl,
+          faviconUrl: response.faviconUrl,
+          urlId: response.$id,
+        });
         return;
       }
     } catch (error) {
-      if (error?.type === responseErrorType.GENERAL_RATE_LIMIT_EXEED) {
-        notify({
-          message: "Too Many Requests! Please try again after some time.",
-          type: responseStatus.ERROR,
-          timeout: 5000,
-        });
-        return;
-      }
-
-      if (error?.type === responseErrorType.DOCUMENT_ALREADY_EXISTS) {
-        notify({
-          message:
-            "Custom back-half slug is already exist. Try to generate a unique slug or skip it.",
-          type: responseStatus.ERROR,
-          timeout: 5000,
-        });
-        return;
-      }
-
-      notify({
-        message: "Failed to create link. Try again later.",
-        type: responseStatus.ERROR,
-        timeout: 5000,
-      });
+      handleApiErrors(error);
     } finally {
       setLoading(false);
     }
   };
 
-  //! UPDATE EXISTING DOCUMENT
+  //* UPDATE EXISTING DOCUMENT
   const updateExistingLinkUrl = async (data) => {
-    const SHORT_URL_DOMAIN = "short.ly";
+    const { urlHostname, faviconUrl } = generateFavicon(data.originalUrl);
 
-    if (data.customSlug === "") {
-      const slug = generateSlug();
-      data.customSlug = slug;
-      data.shortUrl = SHORT_URL_DOMAIN + "/" + data.customSlug;
-    }
+    const updatedUrl = {
+      title: data.title,
+      originalUrl: data.originalUrl,
+      originalUrlDomain: urlHostname,
+      faviconUrl,
+      qrCode: qrCode,
+    };
 
     try {
-      const response = await updateUrlById(
-        {
-          shortUrl: data.shortUrl,
-          title: data.title,
-          customSlug: data.customSlug,
-          originalUrl: data.originalUrl,
-        },
-        urlId
-      );
+      const response = await updateUrlById(updatedUrl, urlId);
       if (response) {
         notify({
           message: "Short url updated successfully.",
@@ -193,37 +169,51 @@ const DashbaordLinkForm = ({ defaultData }) => {
         return;
       }
     } catch (error) {
-      if (error?.type === responseErrorType.GENERAL_RATE_LIMIT_EXEED) {
-        notify({
-          message: "Too Many Requests! Please try again after some time.",
-          type: responseStatus.ERROR,
-          timeout: 5000,
-        });
-        return;
-      }
-
-      if (error?.type === responseErrorType.DOCUMENT_ALREADY_EXISTS) {
-        notify({
-          message:
-            "Custom back-half slug is already exist. Try to generate a new slug",
-          type: responseStatus.ERROR,
-          timeout: 5000,
-        });
-        return;
-      }
-
-      notify({
-        message: "Failed to update link. Try again later.",
-        type: responseStatus.ERROR,
-        timeout: 5000,
-      });
+      handleApiErrors(error);
     } finally {
       setLoading(false);
     }
   };
 
+  //* HANDLE API RESPONSE ERROR
+  const handleApiErrors = (error) => {
+    if (error?.type === responseErrorType.GENERAL_RATE_LIMIT_EXEED) {
+      notify({
+        message: "Too many requests! Please try again later.",
+        type: responseStatus.ERROR,
+        timeout: 5000,
+      });
+    } else if (error?.type === responseErrorType.DOCUMENT_ALREADY_EXISTS) {
+      notify({
+        message: "Custom back-half slug already exists. Try another.",
+        type: responseStatus.ERROR,
+        timeout: 5000,
+      });
+    } else {
+      notify({
+        message: "Failed to save link. Please try again later.",
+        type: responseStatus.ERROR,
+        timeout: 5000,
+      });
+    }
+  };
+
   return (
     <section className="relative w-full h-full flex items-center justify-center">
+      <div
+        className={`fixed inset-0 w-full h-full flex items-center justify-center z-50 bg-white duration-300 ${
+          isEnableSharePopup
+            ? "z-50 opacity-100 pointer-events-auto"
+            : "z-0 opacity-0 pointer-events-none"
+        }`}
+      >
+        <ShareLinkPopup
+          qrCodeSrc={shareDetails.qrCode}
+          link={shareDetails.shortUrl}
+          faviconSrc={shareDetails.faviconUrl}
+          onClose={() => navigate("/dashboard/link/" + shareDetails.urlId2)}
+        />
+      </div>
       <form onSubmit={handleSubmit(onSubmit)} className="space-y-16 w-full">
         <div className="flex items-start gap-4">
           {/* BOX LEFT */}
@@ -243,7 +233,7 @@ const DashbaordLinkForm = ({ defaultData }) => {
             <Input
               label="Original Url"
               type="url"
-              placeholder="https://example.com/my-long-url"
+              placeholder="https://example.com/blog/some-blog-title"
               error={errors?.originalUrl}
               errorMessage={errors?.originalUrl?.message}
               {...register("originalUrl", {
@@ -255,15 +245,37 @@ const DashbaordLinkForm = ({ defaultData }) => {
               })}
             />
 
-            <div className="space-y-2">
-              <label>Custom Back-Half (Optional)</label>
+            <div
+              title={
+                isEditMode
+                  ? "Only title and original url can be editable."
+                  : undefined
+              }
+              className="space-y-2"
+            >
+              <div className="flex items-end justify-between">
+                <label>Custom Back-Half (Optional)</label>
+                {isEditMode && <Lock size={18} className="opacity-50" />}
+              </div>
               <div className="relative flex w-full overflow-hidden">
-                <p className="border border-black border-r-0 py-3 px-5 h-fit bg-zinc-100">
-                  short.ly
-                </p>
+                {!isEditMode && (
+                  <p className="border border-black border-r-0 py-3 px-5 h-fit bg-zinc-100">
+                    short.ly
+                  </p>
+                )}
                 <Input
                   type="text"
-                  placeholder="Eg: your custom back-half"
+                  readOnly={isEditMode}
+                  className={
+                    !isEditMode
+                      ? "bg-white"
+                      : "bg-zinc-100 outline-none cursor-not-allowed font-bormal"
+                  }
+                  placeholder={
+                    isEditMode
+                      ? "short.ly/" + defaultData.customSlug
+                      : "short.ly/custom-back-half"
+                  }
                   error={errors?.customSlug}
                   errorMessage={errors?.customSlug?.message}
                   {...register("customSlug", {
@@ -276,96 +288,79 @@ const DashbaordLinkForm = ({ defaultData }) => {
               </div>
             </div>
           </div>
+
           {/* BOX RIGHT */}
-          {/* <div className="w-[40%] border border-zinc-300 bg-white">
-            <div className="p-6 border-b space-y-4 border-zinc-300">
-              <div className="flex items-center gap-4">
-                <input
-                  type="checkbox"
-                  {...register("expiration")}
-                  onChange={(e) => setExpirationVisible(e.target.checked)}
-                  className="h-5 w-5 accent-black"
+          <div className="w-[40%] h-full border border-zinc-300 bg-white p-8 space-y-4">
+            <p className="">Generate QR Code</p>
+            <div className="p-4 border w-full h-[244px] border-zinc-300 bg-white flex items-center justify-center">
+              {qrCode ? (
+                <img
+                  draggable={false}
+                  loading="lazy"
+                  src={qrCode}
+                  alt="QR Code"
+                  className="w-[150px] h-auto"
                 />
-                <label className="font-medium">Set Expiration</label>
-              </div>
+              ) : (
+                <div className="text-center space-y-2 w-full h-full bg-zinc-100 flex flex-col items-center justify-center">
+                  <QrCode className="mx-auto" color="black" size={40} />
 
-              {expirationVisible && (
-                <input
-                  type="date"
-                  {...register("expirationDate")}
-                  className="w-full border border-gray-400 p-2 px-4"
-                />
-              )}
-            </div>
-
-            <div className="p-6 space-y-4">
-              <div className="flex items-center gap-4">
-                <input
-                  type="checkbox"
-                  {...register("qr")}
-                  onChange={handleQrToggle}
-                  className="h-5 w-5 accent-black "
-                />
-                <label className="font-medium">Generate QR Code</label>
-              </div>
-
-              {qrVisible && qrCode && (
-                <div className="p-4 border border-zinc-300">
-                  <img src={qrCode} alt="QR Code" className="w-full" />
-                </div>
-              )}
-            </div>
-          </div> */}
-
-          {/* // TODO: UPDATE QR GENERATIO PROCESS AND UI */}
-          <div className="w-[40%] border border-zinc-300 bg-white">
-            <div className="p-6 space-y-4">
-              <div className="flex items-center gap-4">
-                <input
-                  type="checkbox"
-                  {...register("qr")}
-                  onChange={handleQrToggle}
-                  className="h-5 w-5 accent-black "
-                />
-                <label className="font-medium">Generate QR Code</label>
-              </div>
-
-              {qrVisible && qrCode && (
-                <div className="p-4 border border-zinc-300">
-                  <img src={qrCode} alt="QR Code" className="w-full" />
+                  <p className="text-sm max-w-[180px] mx-auto">
+                    Enter the long url to generate a QR code.
+                  </p>
                 </div>
               )}
             </div>
           </div>
         </div>
 
-        <div className=" sticky bottom-0 py-6 flex items-center justify-end gap-2 w-full h-fit  border-t border-t-zinc-300 bg-zinc-100">
-          <button
-            type="button"
-            className="bg-zinc-200 border border-zinc-300 px-6 py-2 cursor-pointer"
-            onClick={handleCancle}
-          >
-            Reset to Default
-          </button>
+        <div
+          className={` sticky bottom-0 py-6 flex items-center ${
+            isEditMode ? "justify-between" : "justify-end"
+          } w-full h-fit  border-t border-t-zinc-300 bg-zinc-100`}
+        >
+          {isEditMode && (
+            <div className="">
+              <p className="text-sm">
+                Created at {new Date(defaultData.$createdAt).toDateString()}
+              </p>
+              {defaultData.$createdAt !== defaultData.$updatedAt && (
+                <p className="text-sm">
+                  Last updated at{" "}
+                  {new Date(defaultData.$updatedAt).toDateString()}
+                </p>
+              )}
+            </div>
+          )}
 
-          <button
-            disabled={!isDirty}
-            type="submit"
-            className={` ${
-              isDirty
-                ? "cursor-pointer bg-black"
-                : "cursor-not-allowed bg-zinc-500 border-zinc-500"
-            } bg-black text-white px-6 py-2 border border-black`}
-          >
-            {loading ? (
-              <div className="flex items-center justify-center gap-3 mr-4">
-                <div className="w-5 h-5 rounded-full border-[3px] border-zinc-500 border-l-transparent animate-spin"></div>
-                <p>{isEditMode ? "Updating" : "Creating Link"}</p>
-              </div>
-            ) : (
-              <p>{isEditMode ? "Update Link" : "Create New Link"}</p>
-            )}
-          </button>
+          <div className="flex items-center gap-2">
+            <button
+              type="button"
+              className="bg-zinc-200 border border-zinc-300 px-6 py-2 cursor-pointer"
+              onClick={handleCancle}
+            >
+              Reset to Default
+            </button>
+
+            <button
+              disabled={!isDirty}
+              type="submit"
+              className={` ${
+                isDirty
+                  ? "cursor-pointer bg-black"
+                  : "cursor-not-allowed bg-zinc-500 border-zinc-500"
+              } bg-black text-white px-6 py-2 border border-black`}
+            >
+              {loading ? (
+                <div className="flex items-center justify-center gap-3 mr-4">
+                  <div className="w-5 h-5 rounded-full border-[3px] border-zinc-500 border-l-transparent animate-spin"></div>
+                  <p>{isEditMode ? "Updating" : "Creating Link"}</p>
+                </div>
+              ) : (
+                <p>{isEditMode ? "Update Link" : "Create New Link"}</p>
+              )}
+            </button>
+          </div>
         </div>
       </form>
     </section>
