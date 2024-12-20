9fcfc3a9c77e7c6ea58c37dd45349896251e5383
SujoyKrHaldar <sujoykrhaldar@gmail.com>
2024-12-20 18:05:45 +0530

    update css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,27 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      "small-mobile": "428px", //xs
+      mobile: "640px", //sm
+      tablet: "768px", //md
+      laptop: "1024px", //lg
+      desktop: "1280px", //xl
+      "large-screen": "1536px", //2xl
+    },
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+      // colors: {
+      //   background: "var(--background)",
+      //   foreground: "var(--foreground)",
+      // },
     },
     container: {
       center: true,
       padding: {
-        DEFAULT: "1rem",
-        sm: "2rem",
-        lg: "4rem",
-        xl: "7rem",
+        DEFAULT: "2rem",
+        tablet: "2rem",
+        laptop: "4rem",
+        desktop: "7rem",
       },
     },
   },
