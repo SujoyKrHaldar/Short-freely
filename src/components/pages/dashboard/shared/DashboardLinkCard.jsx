c5d0d8afe31a95112c282a5e9bd54e57312c9bc7
SujoyKrHaldar <sujoykrhaldar@gmail.com>
2024-12-21 23:49:00 +0530

    update card ui

--- src/components/pages/dashboard/shared/DashboardLinkCard.jsx
+++ src/components/pages/dashboard/shared/DashboardLinkCard.jsx
@@ -17,8 +17,11 @@ function DashboardLinkCard({ data }) {
   };
 
   return (
-    <div className="p-5 bg-white border-[1.5px] border-zinc-100 hover:border-black duration-200 flex items-end justify-between">
-      <div className="flex items-start gap-4">
+    <div
+      className="p-5 mobile:p-8 bg-white border-[1.5px] border-zinc-400 laptop:border-zinc-100 laptop:hover:border-black duration-200 
+    flex flex-col laptop:flex-row items-start laptop:items-end justify-between gap-6 laptop:gap-0"
+    >
+      <div className="flex flex-col mobile:flex-row items-start gap-4">
         <div className="w-10 h-10 rounded-full p-1 border border-zinc-200 bg-zinc-100 mt-1">
           <img
             alt={data?.originalUrlDomain}
@@ -30,21 +33,21 @@ function DashboardLinkCard({ data }) {
           />
         </div>
 
-        <div className="space-y-1">
-          <div className="flex items-center gap-1">
-            <p className="text-lg font-bold first-letter:uppercase">
+        <div className="space-y-3">
+          <div className="tablet:flex items-center gap-1">
+            <p className=" text-2xl  font-bold first-letter:uppercase">
               {data?.title}{" "}
             </p>
-            <p>•</p>
+            <p className="hidden tablet:block">•</p>
             <button
               onClick={handleCopy}
               className=" flex items-center gap-2 group cursor-pointer"
             >
-              <p className="text-blue-700 text-lg font-medium group-hover:underline cursor-pointer">
+              <p className="text-blue-700 text-xl tablet:text-2xl font-medium laptop:group-hover:underline cursor-pointer">
                 {data?.shortUrl}
               </p>
               <Copy
-                className="opacity-60 group-hover:opacity-100 duration-300"
+                className="opacity-60 laptop:group-hover:opacity-100 duration-300"
                 color="blue"
                 size={16}
               />
@@ -54,7 +57,7 @@ function DashboardLinkCard({ data }) {
           <Link
             to={data?.originalUrl}
             title={data?.originalUrl}
-            className="text-sm text-zinc-500 hover:underline"
+            className=" text-zinc-500 hover:underline hidden mobile:block"
           >
             {data.originalUrl.slice(0, 50)}...
           </Link>
@@ -67,24 +70,24 @@ function DashboardLinkCard({ data }) {
         </div>
       </div>
 
-      <div className="flex items-center gap-2">
+      <div className="flex flex-col mobile:flex-row mobile:items-center w-full laptop:w-max tablet:justify-end laptop:justify-start gap-2">
         <Link
           to={`/dashboard/link/${data.$id}#analytics`}
-          className="p-2 flex items-center gap-1 bg-white border border-zinc-300 duration-300 text-sm font-medium"
+          className="pt-0 mobile:px-6 py-4 mobile:py-3 flex items-center gap-1 bg-white mobile:border border-zinc-300 duration-300 text-sm font-medium"
         >
           <MousePointerClick className="opacity-50" color="black" size={15} />
           <p>{data?.analyticsCount?.length} clicks</p>
         </Link>
 
         <Link
-          className="px-4 py-2 border border-zinc-300 hover:border-black duration-200 text-sm font-medium"
+          className="px-6 py-4 mobile:py-3 border border-zinc-300 hover:border-black duration-200 text-sm font-medium"
           to={`/dashboard/edit/${data.$id}`}
         >
           Edit or Delete
         </Link>
 
         <Link
-          className="px-4 py-2 bg-zinc-200 border border-zinc-300 hover:border-black duration-300 text-sm font-medium"
+          className="px-6 py-4 mobile:py-3  bg-zinc-200 border border-zinc-300 hover:border-black duration-300 text-sm font-medium"
           to={`/dashboard/link/${data.$id}`}
         >
           View Details
