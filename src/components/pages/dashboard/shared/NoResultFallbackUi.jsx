ef739173be004293679945f0cd3e408d742b49b6
SujoyKrHaldar <sujoykrhaldar@gmail.com>
2024-12-22 11:12:31 +0530

    Update ui for templete

--- src/components/pages/dashboard/shared/NoResultFallbackUi.jsx
+++ src/components/pages/dashboard/shared/NoResultFallbackUi.jsx
@@ -4,12 +4,13 @@ import { emptyImgUrl } from "../../../../utils/imageUrls";
 
 function NoResultFallbackUi({ title, description }) {
   return (
-    <section className="p-8 py-16 w-full h-full flex items-center justify-center bg-white border border-zinc-300">
+    <section className="p-4 tablet:p-8 pt-8 tablet:py-16 w-full h-full flex items-center justify-center bg-white border border-zinc-300">
       <div className="text-center max-w-md">
         <img
           src={emptyImgUrl}
           alt="no result image"
           draggable={false}
+          loading="lazy"
           className="w-[30%] mx-auto"
         />
 
@@ -17,7 +18,7 @@ function NoResultFallbackUi({ title, description }) {
         <p className="mt-2 mb-6 max-w-xs mx-auto text-sm">{description}</p>
 
         <Link
-          className="bg-white text-black px-5 pl-4 py-2 border border-black inline-block"
+          className="bg-white text-black px-5 pl-4 py-2 border border-black inline-block w-full tablet:w-fit"
           to="/dashboard/create"
         >
           <span className="pr-2">+</span> Create a short Link
